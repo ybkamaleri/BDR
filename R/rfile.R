03d65c17d764c3ca035bcf8bfc11433f6b8ab7ba
yuskam <ybkamaleri@gmail.com>
2016-12-01 16:26:25 +0100

    update

---
+++ R/rfile.R
@@ -0,0 +1,44 @@
+##' Konvertering datasettet til R format
+##'
+##' Denne funksjon skal konvertere datafil fra forskjellige formater f.eks csv eller sav
+##' til en format som R kan behandle
+##'
+##' @param filnavn Filnavn hentet fra register database
+##' @return Et dataset i R data.frame format
+##'
+##' @export
+
+
+rfile <- function(filnavn = NULL) {
+
+    ## Hente fil og konvertert til R data.frame
+    if (is.null(filnavn)) { filnavn = Filnavn }
+    filType <- tools::file_ext(filnavn)
+
+    ## SPSS file
+    if (filType == "sav") {
+        dataFile <- foreign::read.spss(filnavn, to.data.frame = TRUE, reencode = "UTF-8")
+    }
+
+    ## DAT file - tab-delimited
+    if (filType == "dat") {
+        dataFile <- read.table(filnavn,
+                               header = TRUE,
+                               encoding = 'UTF-8',
+                               stringsAsFactors = TRUE,
+                               na.strings = "EMPTY")}
+
+    ## CSV file
+    if (filType == "csv") {
+        dataFile <- read.csv(filnavn,
+                            header = TRUE,
+                            encoding = 'UTF-8',
+                            sep=";",
+                            strip.white = TRUE,
+                            stringsAsFactors = TRUE,
+                            na.strings = "EMPTY") #indicate empty as NA
+    }
+
+return(invisible(dataFile))
+
+}
