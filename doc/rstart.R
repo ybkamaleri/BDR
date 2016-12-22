9ca1e10bf353eab374d5bdc628f05bdffa694660
yuskam <ybkamaleri@gmail.com>
2016-12-22 16:25:09 +0100

    restructure Landet og Lokal

--- doc/rstart.R
+++ doc/rstart.R
@@ -23,21 +23,35 @@ Filnavn <- "annonym20161107.sav" 	# *.sav, *.csv, *.xlsx
 
 MinAlder = 2
 MaxAlder = 20
-DBType = 1  # 1:Type1 2:Alle
+DBType = 1      # 1:Type1 2:Alle
 Kjonn  = 3  		# 1:gutt 2:jente 3:alle
-DataValg = 4 		# 1:Førstegangsreg 2:Årskontroll 3:Poliklinisk 4:Alle
+
 DatoFra = "2000-01-01"  # YYYY-MM-DD
 DatoTil = "2016-12-31"  # YYYY-MM-DD
 
+DataValg = 4 		# 1:Førstegangsreg 2:Årskontroll 3:Poliklinisk 4:Alle
+
 #### Rapport ####
 
-Sykehus = 1
+Sykehus = 1         # finnes fra standard kode fra registeret kodebook
 RapportValg = 1 		# 1:Landet 2:Lokal 3:Lokal mot andre sykh.
 
-YAksen = 2 			# 1:prosent, 2:antall, 3:hbalc 4:diabetesVarighet
+YAksen = 2 			    # 1:prosent, 2:antall, 3:hbalc 4:diabetesVarighet
 
 ValgtVar = "alderkat"  	# Valg variablene p? listen nedenfor
 ## alder, alderkat, kjonn,
 
 ## Kjør
-rfigur()
+rfigure()
+
+
+
+
+
+## Omdefinere variablene for test
+sykehus <- Sykehus
+rapvalg <- RapportValg
+yAksen  <- YAksen
+
+library(dplyr)
+library(ggplot2)
