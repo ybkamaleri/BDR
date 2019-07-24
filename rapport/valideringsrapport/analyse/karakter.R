8da3b22a27993298dd4a2739e69ab9bbad494047
BDR admin <yuskam@ous-hf.no>
2019-07-24 17:12:42 +0200

    pasient karakteristika for lokal

--- rapport/valideringsrapport/analyse/karakter.R
+++ rapport/valideringsrapport/analyse/karakter.R
@@ -1,11 +1,14 @@
 ## Kun DT1 pasienter
 ## -----------------
+## Datasettet som skal brukes er dt1
+## for bare lokal sykehus bruker lokal2018
 
 ## Pasient karakteristika
 
 varKar <- c("PasientID", "alder", "diagAlder", "diagVar", "bmi")
 
-dtKar <- subset(dt1, select = varKar)
+## dtKar <- subset(dt1, select = varKar)
+dtKar <- subset(lok2018dt1, select = varKar)
 
 karLong <- melt(dtKar, id.vars = "PasientID", variable.name = "chr", value.name = "tall")
 
@@ -16,7 +19,7 @@ karTall <- karLong[!is.na(tall), .N, by = chr]
 karTab <- karLong[, list(mean = mean(tall, na.rm = T),
                median = median(tall, na.rm = T),
                min = min(tall, na.rm = T),
-               max = max(tall, na.rm = T)), by = .(chr)]
+               maks = max(tall, na.rm = T)), by = .(chr)]
 
 kar.tab <- karTall[karTab, on = "chr"]
 
@@ -26,5 +29,9 @@ for (i in karNavn){
   kar.tab[, (i) := round(get(i), digits = 1)]
 }
 
-# kar.tab[]
+karOld <- varKar[-1]
+karNy <- c("Alder (år)", "Alder ved diagnose (år)", "Sykdomsvarighet (år)", "BMI")
+kar.tab[.(chr = karOld, to = karNy), on = "chr", chr := i.to]
 
+setnames(kar.tab, c("chr", "N", "mean"), c(" ", "n", "gj.snitt"))
+## kar.tab[]
