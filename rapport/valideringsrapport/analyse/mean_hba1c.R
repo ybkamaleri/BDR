38d7a65c0e15b84da48c133dc44846b106c20a43
BDR admin <yuskam@ous-hf.no>
2019-07-24 18:38:39 +0200

    nasjonalitet

--- rapport/valideringsrapport/analyse/mean_hba1c.R
+++
@@ -1,7 +0,0 @@
-## Gjennomsnitt hba1c målt sentralt
-## --------------------------------
-
-## all data
-alldt <- readRDS("../Allebdr_utvalg.RDS")
-
-## OBS! kombling av sykehus først
--- rapport/valideringsrapport/analyse_alldt/nasjonal.R
+++ rapport/valideringsrapport/analyse_alldt/nasjonal.R
@@ -3,14 +3,17 @@
 
 nordLand <- c("Norge", "Danmark", "Finland", "Sverige", "Island")
 
-ars2018[!is.na(fodelandMor) , nordiskMor := ifelse(fodelandMor %in% nordLand, 1L, 0L)]
-ars2018[!is.na(fodelandFar) , nordiskFar := ifelse(fodelandFar %in% nordLand, 1L, 0L)]
+## Data kilder
+nasDT <- ars2018
+
+nasDT[!is.na(fodelandMor) , nordiskMor := ifelse(fodelandMor %in% nordLand, 1L, 0L)]
+nasDT[!is.na(fodelandFar) , nordiskFar := ifelse(fodelandFar %in% nordLand, 1L, 0L)]
 
 ## nordisk barn
 ## Minst en av foreldrene er født i de nordiske landene
-ars2018[, .N, by=.(nordiskFar)]
+nasDT[, .N, by=.(nordiskFar)]
 
-nordBarn <- ars2018[, {mor = ifelse(is.na(nordiskMor), 0, nordiskMor);
+nordBarn <- nasDT[, {mor = ifelse(is.na(nordiskMor), 0, nordiskMor);
              far = ifelse(is.na(nordiskFar), 0, nordiskFar);
              barn = mor + far;
              nordiskBarn = ifelse(is.na(nordiskMor) & is.na(nordiskFar), NA, barn);
@@ -25,5 +28,23 @@ nordBarn <- ars2018[, {mor = ifelse(is.na(nordiskMor), 0, nordiskMor);
 nordBarn[.(nordiskBarn = 1:2, to = 1), on = "nordiskBarn", nordiskBarn := i.to]
 # nordBarn[nordiskBarn %in% 1:2, nordiskBarn := 1]
 
-nordBarn[, .N, by = .(nordiskBarn, kjonn)]
+# ukjent nasjonalitet
+nordBarn[is.na(nordiskBarn), nordiskBarn := 3]
 
+## Tabell long
+barnLg <- nordBarn[, .N, by = .(nordiskBarn, kjonn)]
+
+## Legger prosent
+barnLg[, pros := round(N / sum(N, na.rm = T) * 100, digits = 1)]
+
+## Mix antall og prosent
+barnLg[, n := sprintf("%s (%s%%)", N, pros)]
+
+## Snu til wide
+barnWt <- dcast(barnLg, kjonn ~ nordiskBarn, value.var = "n")
+
+## reorder columns
+setcolorder(barnWt, c("kjonn", "1", "0", "3"))
+
+## legger riktig colnavn
+setnames(barnWt, names(barnWt), c("Kjønn", "Nordisk", "Ikke Nordisk", "Ukjent"))
