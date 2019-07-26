2b0db7a81bc2bee24f3ea016027c149ad0131f61
BDR admin <yuskam@ous-hf.no>
2019-07-26 14:40:10 +0200

    data source

--- rapport/valideringsrapport/analyse_alldt/ispadguide.R
+++ rapport/valideringsrapport/analyse_alldt/ispadguide.R
@@ -1,15 +1,48 @@
 ## ISPAD guidelines
 ## ----------------
 
-ars2018[, ispad := 0]
-ars2018[, ispad := ifelse(alder >=10 & diagVar >=2, 1, 0)]
-ars2018[, ispad := ifelse(alder < 10 & diagVar >=5, 1, ispad)]
+## Datakilde
+ispDT <- ars2018
 
-ars2018[, .N, by=ispad]
+ispDT[, ispad := 0]
+ispDT[, ispad := ifelse(alder >=10 & diagVar >=2, 1, 0)]
+ispDT[, ispad := ifelse(alder < 10 & diagVar >=5, 1, ispad)]
+
+ispDT[, .N, by=ispad]
 
 ## Øye undersøkelse
-ars2018[ispad == 1, .N, by=.(und_Oye)]
+oye <- ispDT[ispad == 1 & und_Oye == "Ja", .(var = "oye", ja = .N)]
+ispDT[ispad == 1, .N, by=.(und_Oye)] #sjekk tallene er riktig
 
 ## Urin
-ars2018[ispad == 1, sum(!is.na(lab_res_1prove))]
-ars2018[ispad == 1, sum(is.na(lab_res_1prove))]
+urin.isp <- ispDT[ispad == 1, sum(!is.na(lab_res_1prove))] #utført
+## ispDT[ispad == 1, sum(is.na(lab_res_1prove))]
+
+urin <- data.table(var = "urin", ja = urin.isp)
+
+## ispad tabell
+ispTab <- rbindlist(list(oye, urin))
+ispTab[, tot := nrow(ispDT)]
+ispTab[, pros := round(ja / tot * 100, digits = 1)]
+
+ispTab[.(var = c("oye", "urin"), to = c("Øye", "Urin")), on = "var", var := i.to]
+setcolorder(ispTab, c("var", "ja", "pros", "tot"))
+setnames(ispTab, names(ispTab), c("Undersøkelser", "Antall", "Andel", "N"))
+
+## lage tabell
+lastLine <- nrow(ispTab) + 1
+isp.htab <- as_hux(ispTab, add_colnames = TRUE)
+
+isp.htab <- isp.htab %>%
+  set_bold(1, everywhere, TRUE) %>%
+  set_bottom_border(lastLine,, TRUE) %>%
+  map_background_color(by_rows("grey95", "white")) %>%
+  set_position("left") %>%
+  set_latex_float("h")
+
+isp.htab <- rbind(c("", "Undersøkelser utført", "", ""), isp.htab)
+
+isp.htab <- merge_cells(isp.htab, 1, 2:3)
+align(isp.htab)[1, 2] <- "center"
+bottom_border(isp.htab)[1, 2] <- 0.5
+top_border(isp.htab)[3, ] <- TRUE
