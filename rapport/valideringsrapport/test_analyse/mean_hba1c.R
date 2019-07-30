b676da165fbbd77aab3c8f464aa6b635419c7ddf
BDR admin <yuskam@ous-hf.no>
2019-07-30 18:08:35 +0200

    mean hba1c for alle

--- rapport/valideringsrapport/test_analyse/mean_hba1c.R
+++ rapport/valideringsrapport/test_analyse/mean_hba1c.R
@@ -33,3 +33,35 @@ alldt <- readRDS(file.path(dataSti, "all07til18.rds"))
 
 ## subset fra loop data
 dtvalg <- subset(alldt, hospid == hosp)
+
+## gjennomsnitt hba1c
+dtvalg[, meanhbc  := round(mean(hba1c, na.rm = TRUE), digits = 1), by = yr]
+
+## antall per år
+dtvalg[, N := .N, by = yr]
+dtvalg[, .N, by = yr]
+
+dtplot <- dtvalg[dtvalg[, .I[1], by = yr]$V1]
+## dtplot <- dtvalg[, head(.SD, 1), yr] #alternativ med litt treg
+
+dbaggr <- rollup(dtvalg, j = .(hba = mean(hba1c, na.rm = TRUE),
+  n = .N), by = c("yr", "kjonn"))
+
+## legger totallen dvs. 3. Kjønn er søm flgende:
+## 1 = Gutter
+## 2 = Jenter
+## 3 = Totalt
+dbaggr[is.na(kjonn), kjonn := 3]
+
+## tar bort Grand Total
+dbaggr <- dbaggr[!is.na(yr), ]
+
+## konverterer yr til nummeric ellers må bruke group=1
+dbaggr[, yr := as.numeric(yr)]
+
+## Plotting
+ggplot(dbaggr, aes(yr, hba, group = kjonn)) +
+  geom_line(aes(color = factor(kjonn))) +
+  geom_point(aes(shape = factor(kjonn)), stroke = 0) +
+  scale_x_continuous(breaks = unique(dbaggr$yr)) +
+  labs(title = "Gjennomsnitt HbA1c", x = " ", y = "HbA1c verdi i %")
