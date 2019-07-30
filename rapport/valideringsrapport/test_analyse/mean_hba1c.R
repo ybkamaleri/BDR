e076c7e94ce24f1c14cca743c00011ed89b86c74
BDR admin <yuskam@ous-hf.no>
2019-07-30 15:19:28 +0200

    hba1c mix

--- rapport/valideringsrapport/test_analyse/mean_hba1c.R
+++ rapport/valideringsrapport/test_analyse/mean_hba1c.R
@@ -1,7 +1,35 @@
 ## Gjennomsnitt hba1c målt sentralt
 ## --------------------------------
 
-## all data
-alldt <- readRDS("/Allebdr_utvalg.RDS")
+## ## Datakilder
+## ## gamle data "bdrB4" uploaded fra runEnkelRapport
+## bdrold <- subset(bdrB4, select = c("hba1c", "yr", "hospid", "age", "gender"))
+## bdrold
 
-## OBS! kombling av sykehus først
+## ## Lokal data
+## dtlokal <- ars2018
+
+## valgVar <- c("lab_HbA1cAkerVerdi", "Kjonn", "yr", "hospID", "alder")
+## dtlokal[, str(.SD), .SDcols = valgVar]
+
+## valgDT <- subset(dtlokal, select = valgVar)
+## valgDT[.(Kjonn = c("Gutt", "Jente"), to = 1:2), on = "Kjonn", gender := i.to]
+## valgDT[, Kjonn := NULL]
+
+## ## gir standard navn
+## stdNavn <- c("hba1c", "yr", "hospid", "alder", "kjonn")
+## setnames(valgDT, names(valgDT), stdNavn)
+## setnames(bdrold, names(bdrold), stdNavn)
+
+## ## Merge begge
+## alldt <- rbindlist(list(bdrold, valgDT), use.names = TRUE)
+
+## ## Lage file til raskere upload
+## saveRDS(alldt, file.path(dataSti, "all07til18.rds"))
+
+
+## upload data
+alldt <- readRDS(file.path(dataSti, "all07til18.rds"))
+
+## subset fra loop data
+dtvalg <- subset(alldt, hospid == hosp)
