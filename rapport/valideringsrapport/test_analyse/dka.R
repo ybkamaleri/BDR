4982a52b05d221755ce06dd2d867629aa7cedb03
BDR admin <yuskam@ous-hf.no>
2019-07-31 13:59:38 +0200

    dka

---
+++ rapport/valideringsrapport/test_analyse/dka.R
@@ -0,0 +1,33 @@
+## Diabetes ketoacidose (DKA)
+
+## Datakilde
+lokalDT <- ars2018
+
+lokalDT[, .N, by = und_ketoacidose]
+
+## minAge <- floor(as.numeric(min(lokalDT$alder, na.rm = TRUE)))
+## maxAge <- ceiling(as.numeric(max(lokalDT$alder, na.rm = TRUE)))
+
+lokalDT[!is.na(alder), agecat := ageCat(alder, 0, 15, by = 5)]
+## lokalDT[, .N, by = agecat]
+## lokalDT[agecat == "15+", .(alder, agecat)]
+
+dkaTab <- groupingsets(lokalDT,
+  j = .(N = sum(!is.na(und_ketoacidose)),
+       ja = length(which(und_ketoacidose == "Ja"))),
+  by = c("agecat"),
+  sets = list(c("agecat"), character(0))
+)
+
+dkaTab[, pros := round(ja / N * 100, digits = 1)]
+dkaTab[ja == 0, pros := "-"]
+
+## lager id til sortering
+dkaTab[, id := .I]
+dkaTab[is.na(agecat), id := 0]
+setorder(dkaTab, -id)
+
+dkaTab[is.na(agecat), agecat := "Totalt"]
+dkaTab[, id := NULL]
+
+tabFun(dkaTab, "Har hatt ketoacidose")
