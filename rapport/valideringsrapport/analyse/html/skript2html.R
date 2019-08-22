db4b794c43beb51a4bafb81e7a701545fc729ccb
BDR admin <yuskam@ous-hf.no>
2019-08-22 17:42:32 +0200

    update

--- rapport/valideringsrapport/analyse/html/skript2html.R
+++ rapport/valideringsrapport/analyse/html/skript2html.R
@@ -13,8 +13,15 @@ spin("type_diabetes.R", knit = FALSE, format = "Rmd")
 setwd("~/Git-work/bdr/rapport/valideringsrapport/analyse")
 rfiles <- list.files()[3:5]
 
-sapply(rfiles, function(x) knitr::spin(x, knit = FALSE, precious = TRUE), USE.NAMES = TRUE)
+lapply(rfiles, function(x) knitr::spin(x, knit = FALSE))
 
+rmdfiles <- list.files(pattern = ".Rmd")
+lapply(rmdfiles, function(x) rmarkdown::render(x, output_format = "html_document"))
+lapply(rmdfiles, function(x) knitr::knit2html(x))
+
+## move files
+library(filesstrings)
+lapply(list.files(pattern = ".Rmd"), function(x) file.move(x, "./html/", overwrite = TRUE))
 
 
 
