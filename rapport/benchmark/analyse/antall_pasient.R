cb856cced919f73e44bf52e9d6be045ac9f11658
BDR admin <yuskam@ous-hf.no>
2019-08-23 17:45:25 +0200

    benchmark first commit

---
+++ rapport/benchmark/analyse/antall_pasient.R
@@ -0,0 +1,19 @@
+## Registrerte pasienter
+
+pas2018 <- DT[, .N, by = .(hospID, yr)]
+pas2017 <- dtB4[yr == 2017, .N, by = .(hospid, yr)]
+
+pas.all <- merge(pas2018, pas2017, by.x = "hospID", by.y = "hospid", all.x = TRUE)
+setkey(pas.all, hospID)
+
+pasDT <- sykehusID[pas.all, on = "hospID"]
+setnames(pasDT, c("yr.x", "N.x", "yr.y", "N.y"), c("yr2018", "n2018", "yr2017", "n2017"), skip_absent = TRUE)
+
+## ggplot(pasDT, aes(x = hosKort), stat = 'identity') +
+##   geom_bar(aes(y = n2018)) +
+##   geom_bar(aes(y = n2017))
+
+## raphist
+source("/home/bdr/Git-work/traume/rapport/annualrap/raphist.R")
+
+pasFig <- raphist(pasDT, hosKort, n2018, n2017, lab1 = "2018", lab2 = "2017", leg1 = "2018", leg2 = "2017")
