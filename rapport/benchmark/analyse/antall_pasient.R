7bfb17036b8890b32107100d4ae42b827afc3ae4
BDR admin <yuskam@ous-hf.no>
2019-08-26 11:22:53 +0200

    edit

--- rapport/benchmark/analyse/antall_pasient.R
+++ rapport/benchmark/analyse/antall_pasient.R
@@ -10,8 +10,7 @@ pasDT <- sykehusID[pas.all, on = "hospID"]
 setnames(pasDT, c("yr.x", "N.x", "yr.y", "N.y"), c("yr2018", "n2018", "yr2017", "n2017"), skip_absent = TRUE)
 
 pasFig <- raphist(pasDT, hosKort, n2018, n2017, lab1 = "(2018)", lab2 = "(2017)", leg1 = "2018", leg2 = "2017")
+
 pasCir <- pasFig +
   annotate(geom = "rect", ymin = 100, ymax = 260, xmin = 5, xmax = 9, alpha = .1) +
-  annotate(geom = "text", x = 7, y = 180, label = "Total kompletthet 98%", size = 5, color = "#002b66")
-
-pasCir
+  annotate(geom = "text", x = 7, y = 180, label = "Total kompletthet 98%", size = 6, color = "#002b66")
