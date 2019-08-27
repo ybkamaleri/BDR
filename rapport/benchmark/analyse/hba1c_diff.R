d52745193c82e08df0c86fd79de00d10a54f1912
BDR admin <yuskam@ous-hf.no>
2019-08-27 15:03:44 +0200

    sammenlikne

---
+++ rapport/benchmark/analyse/hba1c_diff.R
@@ -0,0 +1,22 @@
+## Sammenlikne hba1c sentral og lokal
+
+hbaAker <- "lab_HbA1cAkerVerdi"
+hbaLokal <- "lab_HbA1c"
+
+hbaPlot <- ggplot(dt1, aes(get(hbaLokal), get(hbaAker)), stat = 'identity', position = 'identity') +
+  geom_point(aes(color = hosKort,
+    text = paste(
+      ' Sykehus: ', hosKort, '</br>',
+      '</br> Lokalt: ', get(hbaLokal), '</br>',
+      'Sentral: ', get(hbaAker)
+      ))) +
+  scale_y_continuous(name = "HbA1c målt sentral (Aker)", limits = c(5, 15)) +
+  scale_x_continuous(name = "HbA1c målt lokalt", limits = c(5, 15)) +
+  geom_smooth(method = 'lm', color = "red") +
+  scale_color_viridis_d() +
+  theme(legend.position = "none")
+
+## ggplot(dt1, aes({{ hbaAker }}, {{ hbaLokal}}), position = 'identity') +
+##   geom_point()
+
+hbaInt <- plotly::ggplotly(hbaPlot, tooltip = c("text"))
