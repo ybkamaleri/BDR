e1945ebc5c401335cb42ecfec4f916f773756f4f
Yusman Kamaleri <ybkamaleri@gmail.com>
2019-08-28 22:57:42 +0200

    blodsukkermåling

---
+++ rapport/benchmark/analyse/bsukker.R
@@ -0,0 +1,31 @@
+## Blodsukkermåling
+
+varCount <- "beh_ins_kont_blodsukker_reg"
+
+longDT <- rollup(dt1,
+  j = list(
+    N = sum(!is.na(get(varCount))),
+    n = sum(get(varCount) == 'Ja', na.rm = TRUE)
+  ),
+  by = c("hosKort"))
+
+longDT[is.na(hosKort), hosKort := "Hele landet"]
+
+tabRaw <- longDT[, {
+  pros = n / N * 100;
+  ut = sprintf("%s (%0.1f)", n, pros);
+  list(
+    hosKort = hosKort,
+    ut = ut,
+    N = N
+  )
+}]
+
+for (j in seq_len(ncol(tabRaw))){
+  set(tabRaw, which(tabRaw[[j]] == '0 (0.0)'), j = j, value = " -")
+}
+
+tabNavn <- c("", "n (%)", "N")
+setnames(tabRaw, names(tabRaw), tabNavn)
+
+tabOut <- exp.tabel(tabRaw, xcol = c("", " ", ""), total = 2, valgCol = 2, valgAlign = "right", rowHeight = 0.025)
